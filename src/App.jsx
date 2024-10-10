bddb6805c3c96fca3584805f4c457b8a6a9fb0ee
kosei1224 <kosei.plus3993@gmail.com>
2024-10-10 10:29:41 +0900

    1010

--- src/App.jsx
+++ src/App.jsx
@@ -164,8 +164,8 @@ function App() {
           audio={false}
           ref={webcam}
           videoConstraints={{
-            facingMode: "user",
-            //facingMode: "environment"
+            //facingMode: "user",
+            facingMode: "environment"
           }}
           onUserMedia={() => {
             setCameraOK(true);
